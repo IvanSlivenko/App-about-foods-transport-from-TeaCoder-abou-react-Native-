2e970fa8c9ec9a2b2b37620c01727a6a473d5d33
Ivan Slivenko <umanproger@ukr.net>
2026-08-04 23:55:25 +0300

    04-08-2026-2

--- deliveri-app/client/app/services/api/interceptors.api.ts
+++ deliveri-app/client/app/services/api/interceptors.api.ts
@@ -2,5 +2,9 @@ import { API_URL } from "@/config/api.config";
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: API_URL
+    baseURL: API_URL,
+    headers: {
+        'Content-Type' : 'application/json'
+    }
 })
+
