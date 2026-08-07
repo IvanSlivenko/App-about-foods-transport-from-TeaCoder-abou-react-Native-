3b3d1187b827dbf2901e2f35a0bebb118704e565
Ivan Slivenko <umanproger@ukr.net>
2026-08-07 21:38:02 +0300

    07-08-2026-1

--- deliveri-app/client/app/services/api/interceptors.api.ts
+++ deliveri-app/client/app/services/api/interceptors.api.ts
@@ -1,10 +1,20 @@
 import { API_URL } from "@/config/api.config";
 import axios from "axios";
+import { getAccessToken } from "./helper.api";
 
 const instance = axios.create({
     baseURL: API_URL,
     headers: {
         'Content-Type' : 'application/json'
     }
+})
+
+instance.interceptors.request.use(async (config) => {
+    const accesToken = await getAccessToken()
+
+    if(config.headers && accesToken ) 
+        config.headers.Authorization = `Bearer ${accesToken}`
+    
+    return config
 })
 
